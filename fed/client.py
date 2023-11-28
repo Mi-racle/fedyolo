180ea3090a50009a377bb99993d9830281e597d0
Mi-racle <872137422@qq.com>
2023-11-29 02:28:02 +0800

    fed strategy

--- fed/client.py
+++ fed/client.py
@@ -1,16 +1,16 @@
 import os
 import sys
-
-cur_dir = os.path.dirname(os.path.abspath(__file__))
-root = os.path.abspath(os.path.join(cur_dir, '..'))
-sys.path.append(root)
-
 from collections import OrderedDict
 
 import flwr as fl
 import torch
+from flwr.server.strategy import Strategy
 
 from ultralytics import YOLO
+
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+root = os.path.abspath(os.path.join(cur_dir, '..'))
+sys.path.append(root)
 
 DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 DATA_YAML = 'D:/xxs-signs/fedyolo/datasets/raw_images/data.yaml'
@@ -21,6 +21,7 @@ class CifarClient(fl.client.NumPyClient):
     def __init__(self):
         super().__init__()
         self.net = YOLO('yolov8n.yaml')
+        self.map = 1
 
     def get_parameters(self, config):
         return [val.cpu().numpy() for _, val in self.net.state_dict().items()]
@@ -33,12 +34,13 @@ class CifarClient(fl.client.NumPyClient):
     def fit(self, parameters, config):
         self.set_parameters(parameters)
         metrics = self.train(epochs=1, device=DEVICE)
-        return self.get_parameters(config={}), 20, metrics.results_dict
+        return self.get_parameters(config={}), self.map, metrics.results_dict
 
     def evaluate(self, parameters, config):
         self.set_parameters(parameters)
         metrics = self.test(device=DEVICE)
-        return 1 - metrics.results_dict['metrics/mAP50-95(B)'], 20, metrics.results_dict
+        self.map = metrics.results_dict['metrics/mAP50-95(B)']
+        return 1 - metrics.results_dict['metrics/mAP50-95(B)'], self.map, metrics.results_dict
 
     def train(self, epochs, device):
         """Train the network on the training set."""
